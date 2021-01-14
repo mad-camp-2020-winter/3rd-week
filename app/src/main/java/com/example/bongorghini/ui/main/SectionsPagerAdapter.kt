687565fdc9aa145571218e8a96396df55bda7f4f
DESKTOP-QITT7RJ\q <lysuk96@hanyang.ac.kr>
2021-01-15 01:08:56 +0900

    초기 세팅 fragment 추가

--- app/src/main/java/com/example/bongorghini/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/bongorghini/ui/main/SectionsPagerAdapter.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.example.bongorghini.R
+import com.example.bongorghini.fragment.EngineSelectFragment
+import com.example.bongorghini.fragment.OrderListFragment
 
 private val TAB_TITLES = arrayOf(
         R.string.tab_text_1,
@@ -21,8 +23,18 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+//        return PlaceholderFragment.newInstance(position + 1)
+
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = OrderListFragment()
+            1 -> fragment = EngineSelectFragment()
+        }
+        return fragment!!
     }
+
 
     override fun getPageTitle(position: Int): CharSequence? {
         return context.resources.getString(TAB_TITLES[position])
