3d19b4d0970ccb7cb6bd54c8c367bf66476a4e9e
p51lee <p51lee@kaist.ac.kr>
2021-01-15 16:33:46 +0900

    Get location access permission in MainActivity

--- app/src/main/java/com/example/bongorghini/MainActivity.kt
+++ app/src/main/java/com/example/bongorghini/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.bongorghini
 
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
 import androidx.viewpager.widget.ViewPager
 import com.example.bongorghini.ui.main.SectionsPagerAdapter
 
@@ -21,5 +24,16 @@ class MainActivity : AppCompatActivity() {
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
+
+        // 권한 획득 _______________________________________________________________________________
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 없을 경우 권한 재요청
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,  android.Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION,  android.Manifest.permission.ACCESS_COARSE_LOCATION), 100)
+        }
+
     }
+
 }
